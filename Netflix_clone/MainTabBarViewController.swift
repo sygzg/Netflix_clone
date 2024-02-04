17f5f1a785bf4708dbbaf2cd12faba616c928977
sygzg <eezgisaygi@hotmail.com>
2024-02-04 03:27:39 +0300

    Initial Commit

---
+++ Netflix_clone/MainTabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Netflix_clone
+//
+//  Created by Ezgi on 4.02.2024.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+        
+    }
+
+
+}
+
